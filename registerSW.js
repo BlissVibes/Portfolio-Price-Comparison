035737d82c4695669808a6d89efc0fe484c180bc
BlissVibes <BlissVibes@users.noreply.github.com>
2026-02-24 01:52:45 +0000

    deploy: v0.0.1 32cdf076ebebe96a06a25b3c1c542e701eed5142

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Portfolio-Price-Comparison/sw.js', { scope: '/Portfolio-Price-Comparison/' })})}
